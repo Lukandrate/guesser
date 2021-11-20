8ca190ecd1429333eca82bea8aa19054b88bf4fc
Lukandrate <93555325+Lukandrate@users.noreply.github.com>
2021-11-20 13:56:11 +0000

    Update guesser.sh

--- guesser.sh
+++ guesser.sh
@@ -3,18 +3,48 @@ RANDOM=$$
 r=$(($RANDOM%1001))
 clear
 echo "Try to guess a number from 0 to 1000:"
+k=0
+s=1000
 while :
 do
         read input
+        k=$(($k+1))
+        d=$(($r-$input))
+        d=${d#-}
+        s=$(($s-$d*$k))
         if [ $input -lt $r ]
         then
                 echo "x > $input"
+                if [ $s -le 0 ]
+                then
+                        clear
+                        echo "x > $input"
+                        echo "Game over!"
+                        echo "x = $r"
+                        echo "Attempts: $k"
+                        echo "Your score: $s/1000"
+                        break
+                fi
         elif [ $input -gt $r ]
         then
                 echo "x < $input"
+                if [  $s -le 0  ]
+                then
+                        clear
+                        echo "x < $input"
+                        echo "Game over!"
+                        echo "x = $r"
+                        echo "Attempts: $k"
+                        echo "Your score: $s/1000"
+                        break
+                fi
+
         else
-                echo "x = $input"
+                clear
                 echo "You win!"
+                echo "x = $input"
+                echo "Attempts: $k"
+                echo "Your score: $s/1000"
                 break
         fi
 done
